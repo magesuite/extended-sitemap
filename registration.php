dfa0bd5ea5ead2d43e28fdadd507810df40b6e0a
Dariusz Matoga <dariusz.matoga@creativestyle.pl>
2019-09-03 10:06:04 +0000

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComeponentRegistrar::MODULE,
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
     'MageSuite_ExtendedSitemap',
     __DIR__
 );
